c435016dc0f82b3b446f3141377af1e9f9ad3d51
Dror Kessler <dror27.kessler@gmail.com>
2021-01-13 12:16:59 +0200

    remove unused files

--- code/cmis/getdocurls.sh
+++
@@ -1,1 +0,0 @@
-find . -name "Default.aspx" -print | xargs cat | grep '/Birmingham/Document.ashx' | tr "/" "\n" | grep Document.ashx | tr " " "\n" | grep Document | sed 's/"//g' | awk '{printf("https://birmingham.cmis.uk.com/Birmingham/%s\n", $1)}'
